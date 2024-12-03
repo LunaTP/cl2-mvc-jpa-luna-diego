a8b7da4d2953664892302661874a3ad89942de5a
Diego Alejandro Luna Tarapa <diegolt2014@gmail.com>
2024-12-02 20:01:37 -0500

    inicialización del cl2

---
+++ src/main/java/pe/edu/i202223044/cl2_luna_tarapa_diego_alejandro/Cl2LunaTarapaDiegoAlejandroApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202223044.cl2_luna_tarapa_diego_alejandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2LunaTarapaDiegoAlejandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2LunaTarapaDiegoAlejandroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202223044/cl2_luna_tarapa_diego_alejandro/Cl2LunaTarapaDiegoAlejandroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202223044.cl2_luna_tarapa_diego_alejandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2LunaTarapaDiegoAlejandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
